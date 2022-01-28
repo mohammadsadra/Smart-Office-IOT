4750e8c5564a0246ae744a3dc4c740c0aa3af136
imohammadsadra <mohammadsadrahaeri@gmail.com>
2022-01-28 17:39:27 +0330

    Fixed bug in token required.

--- Remote Server/app.py
+++ Remote Server/app.py
@@ -97,6 +97,30 @@ def token_required(f):
   
     return decorated
 
+def localserver_token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+            token = token.split(' ')[1]
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, pv_key, algorithms=["HS256"])
+            print(data)
+        except:
+            return jsonify({'message' : 'Token is invalid !!'}), 401
+        # returns the current logged in users contex to the routes
+        return  f(true, *args, **kwargs)
+  
+    return decorated
+
+
 
 @app.route('/api/office/register', methods=['POST'])
 def registerOffice():
@@ -289,7 +313,7 @@ def registerUser(current_user):
 ##################################################################
 
 @app.route('/api/user/getlight', methods=['GET'])
-@token_required
+@localserver_token_required
 def getLight():
     try:
         body = request.get_json()
@@ -316,7 +340,7 @@ def getLight():
     return resp
  
 @app.route('/api/user/addActivity', methods=['POST'])
-@token_required
+@localserver_token_required
 def addActivityToTable():
     try:
         body = request.get_json()
